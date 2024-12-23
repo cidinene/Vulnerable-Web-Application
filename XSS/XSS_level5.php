3f97589906552963f6e23448dffe4cbdcacecab2
eva42679 <pyi42679@educastur.es>
2024-12-23 15:28:17 +0100

    Update XSS_level5.php

--- XSS/XSS_level5.php
+++ XSS/XSS_level5.php
@@ -22,9 +22,11 @@ if (isset($_GET["username"])) {
     
     $user = htmlspecialchars($_GET["username"], ENT_QUOTES, 'UTF-8');
     
-    echo "Your name is " . $user;
+   
+    echo "Welcome, user! Your request has been received.";
 }
 ?>
+
 
 
 </body>
